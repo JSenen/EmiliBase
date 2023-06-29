c06bf5ee68b495775c610634132d6450161e1e8d
Juan Senen <juansenen@hotmail.com>
2023-06-29 17:32:58 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# EMILIBASE
---
+++ src/main/java/com/juansenen/emilibase/EmilibaseApplication.java
@@ -0,0 +1,13 @@
+package com.juansenen.emilibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmilibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmilibaseApplication.class, args);
+	}
+
+}
